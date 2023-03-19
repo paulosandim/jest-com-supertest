056a8db76b59ebb2de89e2eef8da7f3e7bb89a89
Paulo Sandim <paulosandim@moss.earth>
2023-03-18 22:00:32 -0300

    verifica informações de cadastro

--- __tests__/swapi.spec.js
+++ __tests__/swapi.spec.js
@@ -1,12 +1,18 @@
 const request = require('supertest')
 
-it('', async () => {
+it('Deve visualizar informações de cadastro, quando buscar uma pessoa existente', async () => {
   // https://swapi.dev/api
   // /people/1
 
   const resposta = await request(`https://swapi.dev/api`).get(`/people/1`)
 
-  console.log(resposta.status)
-  console.log(resposta.body)
+  expect(resposta.status).toBe(200)
+  expect(resposta.body.films).toBeDefined()
+  expect(resposta.body.vehicles.length).toBeGreaterThan(0)
+  expect(resposta.body.name).toBe('Luke Skywalker')
 
 })
+
+it('Deve receber uma mensagem de erro, quando buscar por uma pessoa inesxistente', () => {
+  
+});
